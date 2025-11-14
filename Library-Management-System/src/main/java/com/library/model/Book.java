e83714ac0ccd81f1ef946ede4242e7af279af4c4
Jaffar kazmi <kazmijaffar890@gmail.com>
2025-11-14 23:22:54 +0500

    refactor: update Book.java and move to model package

---
+++ Library-Management-System/src/main/java/com/library/model/Book.java
@@ -0,0 +1,76 @@
+package com.library.model;
+
+import java.time.LocalDate;
+
+public class Book {
+    private int bookId;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private LocalDate publicationDate;
+    private String category;
+    private int totalCopies;
+    private int availableCopies;
+
+    public Book(String isbn, String title, String author, String publisher) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.totalCopies = 1;
+        this.availableCopies = 1;
+    }
+
+    public Book(String isbn, String title, String author, String publisher,
+                LocalDate publicationDate, String category, int totalCopies, int availableCopies) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.publicationDate = publicationDate;
+        this.category = category;
+        this.totalCopies = totalCopies;
+        this.availableCopies = availableCopies;
+    }
+
+    // Getters and Setters
+    public int getBookId() { return bookId; }
+    public void setBookId(int bookId) { this.bookId = bookId; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getPublisher() { return publisher; }
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+
+    public LocalDate getPublicationDate() { return publicationDate; }
+    public void setPublicationDate(LocalDate publicationDate) { this.publicationDate = publicationDate; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public int getTotalCopies() { return totalCopies; }
+    public void setTotalCopies(int totalCopies) { this.totalCopies = totalCopies; }
+
+    public int getAvailableCopies() { return availableCopies; }
+    public void setAvailableCopies(int availableCopies) { this.availableCopies = availableCopies; }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookId=" + bookId +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", availableCopies=" + availableCopies +
+                '}';
+    }
+}
